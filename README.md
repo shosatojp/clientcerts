7f082dfe00e1cce12477bac0ab2212458c685bd4
shosatojp <me@shosato.jp>
2022-08-01 14:00:25 +0900

    Upload

---
+++ README.md
@@ -0,0 +1,39 @@
+# Client Certificates
+
+## Usage
+
+```sh
+clientcert.sh ca
+clientcert.sh user user1
+```
+
+## Settings
+
+### Chrome
+
+1. navigate to [chrome://settings/certificates](chrome://settings/certificates)
+2. move to "Your Certificates" tab
+3. click Import and select p12 file (leave password empty)
+
+### Firefox
+
+1. navigate to Settings
+2. search "Certificates" and click "View Certificates"
+3. move to "Your Certificates" tab
+4. click Import and select p12 file (leave password empty)
+
+### Android
+
+1. download p12 file
+2. open downloaded file by file manager
+3. enter password
+4. Certificate name: ANY STRING, Credential use: VPN and apps
+
+### iOS
+
+1. "Save to Files" p12 file
+2. click p12 file in Files app
+3. navigate to Settings app, click Profile Downloaded
+4. install
+
+> manage at Settings -> General -> VPN & Device Management
---
+++ clientcert.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+key_size=4096
+
+clean() {
+    rm -f *.crt *.key *.p12
+}
+
+create_ca() {
+    openssl genrsa -out ca.key ${key_size}
+    # https://datatracker.ietf.org/doc/html/rfc4519#section-2
+    openssl req -new -x509 -key ca.key -out ca.crt -subj "/O=clientcert/CN=CA"
+}
+
+create_user() {
+    username=$1
+
+    openssl genrsa -out ${username}.key ${key_size}
+    openssl req -new -key ${username}.key -out ${username}.csr -subj "/O=clientcert/CN=${username}"
+	openssl x509 -req -in ${username}.csr -CA ca.crt -CAkey ca.key -set_serial $(date +%s) -out ${username}.crt
+	rm ${username}.csr
+	openssl pkcs12 -export -out ${username}.p12 -inkey ${username}.key -in ${username}.crt -certfile ca.crt # -password pass:''
+}
+
+usage() {
+    echo "Usage: clientcert.sh (ca|user) [username]"
+    echo "Example: "
+    echo "    clientcert.sh ca"
+    echo "    clientcert.sh user user1"
+}
+
+if [ "$1" == "ca" ];then
+    clean
+    create_ca
+elif [ "$1" == "user" ] && [ "$2" ];then
+    create_user "$2"
+else
+    usage
+    exit 1
+fi
